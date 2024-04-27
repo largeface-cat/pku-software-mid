e96923c22d5a8c215796444ae18d22147db240f6
largeface-cat <lsh200408@outlook.com>
2024-04-27 11:32:33 +0800

    1

--- citation.h
+++ citation.h
@@ -3,8 +3,35 @@
 #define CITATION_H
 
 #include <string>
+#include <utility>
 
 class Citation {
+private:
+    int type;
+    std::string id{""};
+public:
+    Citation(const int &t, std::string &id) : type(t), id(id) {}
+    std::string getId() { return id; }
 };
 
+class BookCitation : public Citation {
+private:
+    std::string isbn{""}, author{""}, title{""}, press{""}, year{""};
+public:
+    BookCitation(const int &t, std::string &id) : Citation(t, id) {};
+};
+
+class WebCitation : public Citation {
+private:
+    std::string title{""}, url{""};
+public:
+    WebCitation(const int &t, std::string &id) : Citation(t, id) {};
+};
+
+class ArticleCitation : public Citation {
+private:
+    std::string author{""}, title{""}, paper{""}, year{""}, vol{""}, no{""};
+public:
+    ArticleCitation(const int &t, std::string &id) : Citation(t, id) {};
+};
 #endif
--- main.cpp
+++ main.cpp
@@ -5,30 +5,58 @@
 #include "utils.hpp"
 #include "citation.h"
 
-std::vector<Citation*> loadCitations(const std::string& filename) {
+#include "third_parties/nlohmann/json.hpp"
+
+std::vector<Citation *> loadCitations(const std::string &filename) {
     // FIXME: load citations from file
+    std::vector<Citation *> vec_citations;
+    std::ifstream file{filename};
+    nlohmann::json data = nlohmann::json::parse(file);
+    for (auto &item: data["citations"]) {
+        int type;
+        Citation *citation;
+        std::string id = item["id"].get<std::string>();
+        type = (item["type"].get<std::string>() == "book") +
+               2 * (item["type"].get<std::string>() == "webpage") +
+               3 * (item["type"].get<std::string>() == "article");
+        switch (type) {
+            default:
+                std::exit(1);
+            case 1:
+                citation = new BookCitation{1, id};
+            case 2:
+                citation = new WebCitation{2, id};
+            case 3:
+                citation = new ArticleCitation{3, id};
+        }
+        vec_citations.push_back(citation);
+    }
+    return vec_citations;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     // "docman", "-c", "citations.json", "input.txt"
 
     auto citations = loadCitations(argv[2]);
-    std::vector<Citation*> printedCitations{};
+    for (auto c:citations) {
+        std::cout<<c->getId();
+    }
+    std::vector<Citation *> printedCitations{};
 
     // FIXME: read all input to the string, and process citations in the input text
-    // auto input = readFromFile(argv[3]);
+//     auto input = readFromFile(argv[3]);
     // ...
 
-    std::ostream& output = std::cout;
+    std::ostream &output = std::cout;
 
     // output << input;  // print the paragraph first
     // output << "\nReferences:\n";
-    
-    for (auto c : printedCitations) {
+
+    for (auto c: printedCitations) {
         // FIXME: print citation
     }
 
-    for (auto c : citations) {
+    for (auto c: citations) {
         delete c;
     }
 }
