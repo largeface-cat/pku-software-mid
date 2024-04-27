2b853994b0af0ccee45c8384a42ff34cefd6326a
largeface-cat <lsh200408@outlook.com>
2024-04-27 13:43:54 +0800

    main

--- main.cpp
+++ main.cpp
@@ -1,62 +1,136 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-
+#include <exception>
 #include "utils.hpp"
 #include "citation.h"
 
 #include "third_parties/nlohmann/json.hpp"
 
-std::vector<Citation *> loadCitations(const std::string &filename) {
+std::vector<Citation*> loadCitations(const std::string& filename)
+{
     // FIXME: load citations from file
-    std::vector<Citation *> vec_citations;
+    std::vector<Citation*> vec_citations;
     std::ifstream file{filename};
     nlohmann::json data = nlohmann::json::parse(file);
-    for (auto &item: data["citations"]) {
+    for (auto& item : data["citations"])
+    {
         int type;
-        Citation *citation;
+        Citation* citation;
         std::string id = item["id"].get<std::string>();
         type = (item["type"].get<std::string>() == "book") +
-               2 * (item["type"].get<std::string>() == "webpage") +
-               3 * (item["type"].get<std::string>() == "article");
-        switch (type) {
-            default:
-                std::exit(1);
-            case 1:
-                citation = new BookCitation{1, id};
-            case 2:
-                citation = new WebCitation{2, id};
-            case 3:
-                citation = new ArticleCitation{3, id};
+            2 * (item["type"].get<std::string>() == "webpage") +
+            3 * (item["type"].get<std::string>() == "article");
+        switch (type)
+        {
+        default:
+            std::exit(1);
+        case 1:
+            citation = new BookCitation{1, id};
+        case 2:
+            citation = new WebCitation{2, id};
+        case 3:
+            citation = new ArticleCitation{3, id};
         }
         vec_citations.push_back(citation);
     }
     return vec_citations;
 }
 
-int main(int argc, char **argv) {
-    // "docman", "-c", "citations.json", "input.txt"
 
-    auto citations = loadCitations(argv[2]);
-    for (auto c:citations) {
-        std::cout<<c->getId();
+std::string readFromFile(const std::string& filename)
+{
+    std::string content;
+    std::string line;
+    if (filename == "-")
+    {
+        while (std::getline(std::cin, line))
+        {
+            content += line + "\n";
+        }
+        return content;
     }
-    std::vector<Citation *> printedCitations{};
+    std::ifstream file{filename};
+    while (std::getline(file, line))
+    {
+        content += line + "\n";
+    }
+    return content;
+}
+class Output
+{
+private:
+    std::ostream* output;
+public:
+    Output(std::ostream* output) : output{output} {}
+    template <typename T>
+    Output& operator<<(const T& t)
+    {
+        *output << t;
+        return *this;
+    }
+    Output& operator=(std::ofstream* file)
+    {
+        output = file;
+        return *this;
+    }
+};
+int main(int argc, char** argv)
+{
+    // "docman", "-c", "citations.json", "input.txt"
+    if (argc < 4 or argv[1] != std::string{"-c"})
+    {
+        std::cerr << "Invalid arguments" << std::endl;
+        std::exit(1);
+    }
+    std::vector<Citation*> citations{};
+    try
+    {
+        citations = loadCitations(argv[2]);
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::exit(1);
+    }
+    for (auto c : citations)
+    {
+        std::cout << c->getId();
+    }
+    std::vector<Citation*> printedCitations{};
 
     // FIXME: read all input to the string, and process citations in the input text
-//     auto input = readFromFile(argv[3]);
+    std::string input{};
+
+
     // ...
+    Output out {&std::cout};
+    if (argv[3] == "-o")
+    {
+        if (argc != 6)
+        {
+            std::cerr << "Invalid arguments" << std::endl;
+            std::exit(1);
+        }
+        auto os = std::ofstream{argv[4]};
+        out = &os;
+        input = readFromFile(argv[5]);
+    }
+    else
+    {
+        input = readFromFile(argv[3]);
+    }
 
-    std::ostream &output = std::cout;
+    out << input; // print the paragraph first
+    out << "\nReferences:\n";
 
-    // output << input;  // print the paragraph first
-    // output << "\nReferences:\n";
-
-    for (auto c: printedCitations) {
+    for (auto c : printedCitations)
+    {
         // FIXME: print citation
     }
 
-    for (auto c: citations) {
+    for (auto c : citations)
+    {
         delete c;
     }
 }
